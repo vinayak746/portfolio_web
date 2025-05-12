e58215c946928836ff717e559c971a42941c4913
Vinayak Arora <vinayakarora7461@gmail.com>
2025-05-12 11:28:28 +0530

    Update developer location in Hero component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,8 +51,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Vinayak, a developer based in Croatia with a passion for
-              code.
+              Hi, I’m Vinayak, a developer from India with a passion for code.
             </p>
 
             <Button
