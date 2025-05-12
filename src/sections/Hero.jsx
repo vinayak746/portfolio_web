2607ccb398c57d4964a0ae6a1a0ae36e9e5f2578
Vinayak Arora <vinayakarora7461@gmail.com>
2025-05-12 11:23:57 +0530

    Update dependencies and modify personal references in components

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adrian, a developer based in Croatia with a passion for
+              Hi, I’m Vinayak, a developer based in Croatia with a passion for
               code.
             </p>
 
